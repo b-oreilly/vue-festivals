ab2feda28e388b57623edc9dd60963351a68a1ac
b-oreilly <berniceoreilly@gmail.com>
2021-11-11 12:20:27 +0000

    Vue router, BootstrapVue, Nav & Footer added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import {
+  BootstrapVue
+} from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-col>
+    <h2>Welcome to the Cloud Festivals Website</h2>
+  </b-col>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    components: {
+
+    }
+  };
+</script>
